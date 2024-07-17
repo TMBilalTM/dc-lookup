578b82c8de492aaac84dcb8373d57c9f46edb289
Bilal <112407129+TMBilalTM@users.noreply.github.com>
2024-07-17 23:02:01 +0300

    BilalTM

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,13 +9,11 @@ export default function Home() {
   const [value, setValue] = useState<string>('');
   const [user, setUser] = useState<any>(null);
 
-  // Tarihi uygun formatta göstermek için fonksiyon
   const formatDate = (dateString: string) => {
     const date = new Date(dateString);
     const options: Intl.DateTimeFormatOptions = { day: 'numeric', month: 'long', year: 'numeric' };
     return date.toLocaleDateString('tr-TR', options);
   };
-  
 
   async function fetch() {
     const check = await axios.get(`/api/user/${value}`).then(res => res.data).catch(() => null);
@@ -61,7 +59,7 @@ export default function Home() {
   };
 
   return (
-    <div className="h-screen w-full mx-auto flex flex-col items-center justify-center">
+    <div className="min-h-screen w-full mx-auto flex flex-col items-center justify-center p-4 md:p-8">
       <span
         className="w-full block h-[35rem] absolute -z-[1] select-none mask top-0 left-0 right-0"
         draggable={false}
@@ -71,8 +69,10 @@ export default function Home() {
           backgroundPosition: 'center',
         }}
       />
-      <p className="text-5xl text-[#0e172b]/90 font-bold tracking-tighter">Discord Lookup</p>
-      <p className="text-[#0e172b]/60 font-medium tracking-tighter mt-4">Find Discord user easily.</p>
+      <div className="mt-16 text-center">
+        <p className="text-3xl md:text-5xl text-[#0e172b]/90 font-bold tracking-tighter">Discord Lookup</p>
+        <p className="text-[#0e172b]/60 font-medium tracking-tighter mt-4">Find Discord user easily.</p>
+      </div>
       <input
         className="mt-5 w-full max-w-lg py-1.5 rounded-md bg-gray-200/30 backdrop-blur-xl hover:bg-gray-200/40 transition-all duration-200 outline-none px-4 focus:bg-gray-200/40 border border-black/5"
         onChange={(e) => setValue(e.target.value)}
@@ -88,22 +88,22 @@ export default function Home() {
           initial={{ translateY: 50, opacity: 0 }}
           animate={{ translateY: 0, opacity: 1 }}
           transition={{ duration: 0.3 }}
-          className="flex flex-col items-center justify-start w-[42rem] h-[25rem] bg-gray-200/40 mt-12 rounded-3xl">
-          <div className="flex flex-col items-start justify-start">
+          className="flex flex-col items-center justify-start w-full max-w-4xl h-auto bg-gray-200/40 mt-12 rounded-3xl p-4 md:p-8">
+          <div className="flex flex-col items-start justify-start w-full">
             {user?.banner !== null ? (
-              <img src={user?.banner?.startsWith('a_') ? `https://cdn.discordapp.com/banners/${user.id}/${user.banner}.gif?size=4096` : `https://cdn.discordapp.com/banners/${user.id}/${user.banner}.png?size=4096`} alt="Banner" className="w-[40rem] mt-4 rounded-xl object-cover h-[10rem]" />
+              <img src={user?.banner?.startsWith('a_') ? `https://cdn.discordapp.com/banners/${user.id}/${user.banner}.gif?size=4096` : `https://cdn.discordapp.com/banners/${user.id}/${user.banner}.png?size=4096`} alt="Banner" className="w-full h-[10rem] md:h-[15rem] rounded-xl object-cover" />
             ) : (
-              <div className={`w-[40rem] mt-4 rounded-xl h-[10rem] bg-[${user.banner_color ?? '#000000'}]`} />
+              <div className={`w-full h-[10rem] md:h-[15rem] rounded-xl bg-[${user.banner_color ?? '#000000'}]`} />
             )}
-            <div className="flex items-start justify-between w-full mt-4">
+            <div className="flex flex-col md:flex-row items-start md:items-center justify-between w-full mt-4">
               <div className="flex items-center">
-                <img src={user?.modified_avatar} alt="Avatar" className="w-20 h-20 rounded-full" />
+                <img src={user?.modified_avatar} alt="Avatar" className="w-16 md:w-20 h-16 md:h-20 rounded-full" />
                 <div className="ml-4">
-                  <p className="text-[#0e172b]/90 font-semibold tracking-tighter text-xl">{user?.global_name}</p>
+                  <p className="text-[#0e172b]/90 font-semibold tracking-tighter text-lg md:text-xl">{user?.global_name}</p>
                   <p className="text-[#0e172b]/60 font-medium tracking-tighter text-sm">{formatDate(user?.created_at)}</p>
                 </div>
               </div>
-              <div className="flex items-center px-1 bg-gray-200 rounded-md p-0.5">
+              <div className="flex items-center px-1 bg-gray-200 rounded-md p-0.5 mt-4 md:mt-0">
                 {user?.badges_string.map((badge: string) => (
                   <Badge key={badge} badge={badge} />
                 ))}
@@ -116,18 +116,18 @@ export default function Home() {
           </div>
         </motion.div>
       ) : (
-        <div className="flex flex-col items-center justify-start w-[42rem] h-[25rem] bg-gray-200/40 mt-12 rounded-3xl">
-          <div className="flex flex-col items-start justify-start">
-            <img src='https://assets-global.website-files.com/5f9072399b2640f14d6a2bf4/611af00d256b9e541fac258f_0_4clCON4Ko2L_PqGi.png' alt="Banner" className="w-[40rem] mt-4 rounded-xl object-cover h-[10rem]" />
-            <div className="flex items-start justify-between w-full mt-4">
+        <div className="flex flex-col items-center justify-start w-full max-w-4xl h-auto bg-gray-200/40 mt-12 rounded-3xl p-4 md:p-8">
+          <div className="flex flex-col items-start justify-start w-full">
+            <img src='https://assets-global.website-files.com/5f9072399b2640f14d6a2bf4/611af00d256b9e541fac258f_0_4clCON4Ko2L_PqGi.png' alt="Banner" className="w-full h-[10rem] md:h-[15rem] rounded-xl object-cover" />
+            <div className="flex flex-col md:flex-row items-start md:items-center justify-between w-full mt-4">
               <div className="flex items-center">
-                <img src="https://cdn.discordapp.com/embed/avatars/0.png" alt="Avatar" className="w-20 h-20 rounded-full" />
+                <img src="https://cdn.discordapp.com/embed/avatars/0.png" alt="Avatar" className="w-16 md:w-20 h-16 md:h-20 rounded-full" />
                 <div className="ml-4">
-                  <p className="text-[#0e172b]/90 font-semibold tracking-tighter text-xl">Example</p>
+                  <p className="text-[#0e172b]/90 font-semibold tracking-tighter text-lg md:text-xl">Example</p>
                   <p className="text-[#0e172b]/60 font-medium tracking-tighter text-sm">21 Nisan 2021</p>
                 </div>
               </div>
-              <div className="flex items-center px-1 bg-gray-200 rounded-md p-0.5">
+              <div className="flex items-center px-1 bg-gray-200 rounded-md p-0.5 mt-4 md:mt-0">
                 {['ActiveDeveloper', 'HypeSquadOnlineHouse1'].map((badge: string) => (
                   <Badge key={badge} badge={badge} />
                 ))}
